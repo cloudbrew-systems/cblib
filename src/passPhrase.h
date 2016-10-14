41d28a21a0ce4c573baf33209cea8a74bdcc6e2f
Runcy <runcy.oommen@gmail.com>
2016-10-14 15:20:47 +0530

    Scramble the passphrase to make it generic

--- src/passPhrase.h
+++ src/passPhrase.h
@@ -6,11 +6,11 @@ extern "C" {
 #ifndef PASSPHRASE_H
 #define PASSPHRASE_H
 
-/* Secret passphrases per account */
-#define PASSPHRASE_GOOGLEDRIVE	"2xXVKacfC15eiOJFqoVG9fcf7gM5sBLElUKqQslnE7CVXUBsKhuvwb5M21c9NuNVJcUafxyjI1c5qSErNYSOLEcY4P3XLMTrdyri4FIUIv7pxTp7Tt1W4sJ5ZMhLuIaK"
-#define PASSPHRASE_ONEDRIVE		"djVtjqatuzqLria0erb5ckHFG3pfvshbkwHvzPLWHXYYJL2KOBp8OdLoE5HI2eovMajasb5UrmEfTVoAK3irKC18HrshVt2VXDwyDzr93svUXCtSJiQCw73jjKq1Hph4"
-#define PASSPHRASE_BOX				"ckVx63YYNIB5uQlOpTwhGy1uQKThrD4HWawHVhMxfdw8dBUvGiXTbBGnby5IZ5wFBmtPcg9rztvg8qosYBEsjpSh69zhPwLO2Ews5o3RIVGUTfiCKAq4FS7WLcApMBKw"
-#define PASSPHRASE_DROPBOX		"SxNm831H6YwV6PMIVLN8jpbzXW9eJQos2TZep8dBxJJsFCC9fC4nT7aE67HVlcoyuaD1lrihug6N7krYGqIBF1Mjl6wjrcsBIYtyphRJ9D9oCpc2h5uoVyfoTgv8ZFqE"
+/* Secret passphrases per account - change this as per your need - longer the better */
+#define PASSPHRASE_GOOGLEDRIVE	"abcdefghijklmnopqrstuvwxyz1234567890"
+#define PASSPHRASE_ONEDRIVE		"abcdefghijklmnopqrstuvwxyz1234567890"
+#define PASSPHRASE_BOX				"abcdefghijklmnopqrstuvwxyz1234567890"
+#define PASSPHRASE_DROPBOX		"abcdefghijklmnopqrstuvwxyz1234567890"
 
 #endif /* PASSPHRASE_H */
 

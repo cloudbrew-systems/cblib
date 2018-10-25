d8bb082d999023837a11a7d6bb679d5dc48c0ae3
Runcy Oommen <runcy.oommen@gmail.com>
2018-10-26 00:16:05 +0530

    Modifications underway for Files class. Include sha256 logic in C++

--- localhost/src/Files.h
+++ localhost/src/Files.h
@@ -4,13 +4,22 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
+#include <sys/stat.h>
+
+#include "cbDefines.h"
 
 class Files {
 public:
     Files(std::string _filename): filename(_filename) {}
-    int createDir(std::string userId);
+    int createdir(std::string);
+    unsigned long long int sizeinbytes(std::string);
+    std::string shredsuffix(int);
+    std::string getsha256hash(std::string);
 private:
     std::string filename;
+protected:
+    const std::string FILES_TEMPDIR = "/tmp/upload";
 };
 
 #endif /* FILES_H */
